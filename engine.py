80db795af1218c175f733e2b1ea0379412d2c301
c3ypt1c <c3ypt1c@gmail.com>
2016-10-04 19:04:14 +0100

    Change Log:

--- engine.py
+++ engine.py
@@ -18,21 +18,6 @@ class words:
                 tick+= 1
             return [auth, tick]
 
-        def order(word):
-            basket = {}
-            order = [[],[]]
-            tick = 0
-            for x in str( word ):
-                f = dfind(basket, x)
-                if not f[0]:
-                    basket[f[1]] = x
-                order[0].append(f[1])
-                tick += 1
-
-            order[1] = basket
-
-            return order
-
         def prog( x, outOf, size=40, char="/", brackets=True, brackeType = "<>" ):
             per = ( ( x ) / outOf )                       #Percentage of shaded in
             charA = ( ( per ) * size )                    #Undertale :D
@@ -62,9 +47,7 @@ class words:
             print ( "Decoding" )
             fd = fd.decode().lower().split("\r\n")                  #decoding from UTF-8 to string and splitting into actual words
             wordl = []
-                                                                    #Format so I do not forget:
-            combd = {}                                              #combd = {[combination]:[{letters},...  ],  ...}
-                                                                    #letters = { 0:letter, 1:letter,... }
+
             print ( "Success!")
             tttt = time.time() + timePerRefresh
             tick = 0
@@ -72,7 +55,6 @@ class words:
             prog = len ( fd )
             for x in fd:
                 try:
-            
                     try:
                         y = x.lower()                               #Removes capitals that would mess up future code. 
                     except: pass                                    #Better safe than sorry
@@ -87,20 +69,6 @@ class words:
                         except IndexError:                          #only look in the lenghs of the
                             wordl.append([])                        #word and not all of the arrat
                                                                     #which ultimatley optimises the code :D
-                    if not auth:
-                        raise NameError("Word took too long")
-
-                    auth = False
-                    tt = time.time() 
-                    t = fun.order(y)
-                    while tt + timePerWord >= time.time():
-                        try:
-                            combd[str(t[0])].append(t[1])           #THIS WAS combd[str(t[0])].append(t[1][0]) AND WAS A BUG!
-                            auth = True
-                            break
-                        except KeyError:                            #Fixed so that other errors don't ruin the rest of the dict.
-                            combd[str(t[0])]=[]                     #You can't use a list as a key but stringing a list
-                                                                    #is an amazing idea I had :D
                     if not auth:
                         raise NameError("Word took too long")
 
@@ -124,14 +92,6 @@ class words:
     except:
         print ( "Reading Failed" )
 
-    alphadict = {"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,"k":11,"l":12,"m":13,"n":14,"o":15,"p":16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26}
-
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".lower()
-    letters = []
-    for x in alphabet: letters.append(x)
-
-
-
 ##Yes/ No interface
 def yn(ss="Y/N> "):
     s = input(ss).lower()[0];
@@ -147,6 +107,7 @@ def menu():
 while True:
     print ( menu() );
     yn();
+    
 
 
 
