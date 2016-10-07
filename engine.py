459417ad7cff437d1ef46a5bc6f1860898f57cbe
c3ypt1c <c3ypt1c@gmail.com>
2016-10-07 12:11:36 +0100

    Change Log:
    - Changed it into a function.
    - Added semicolons that I missed before.

--- engine.py
+++ engine.py
@@ -56,10 +56,10 @@ class words:
                 try:
                     try:
                         y = x.lower();                                  #Removes capitals that would mess up future code. 
-                    except: pass                                        #Better safe than sorry
+                    except: pass;                                       #Better safe than sorry
                 
                     tt = time.time();                                   #Unknown Hangs :( Program will skips some words
-                    auth = False
+                    auth = False;
                     while tt + timePerWord >= time.time():
                         try:
                             wordl[len(y)].append(y);                    #Sorting into word lenghs
@@ -109,42 +109,34 @@ def menu():
 ##    yn();
     
 
-##Generate signs
-##       + - * /
-signs = [1,2,3,4];
-numbs = [[1,2,3,4,5,6],7];
-hrn = {1:"+", 2:"-", 3:"*", 4:"/"};
-while True:
-    try:
-        hrm = {1:eval(input("1> ")), 2:eval(input("2> ")), 3:eval(input("3> ")), 4:eval(input("4> ")), 5:eval(input("5> ")), 6:eval(input("6> ")), 7:eval(input("Target> "))};
-        break
-    except:
-        pass;
-    
-sol = []
-i = 0;
-while i != 6:
-    for x in itertools.permutations(numbs[0], i+1):
-        x = list( x );
-        for y in itertools.product(signs, repeat=i):
-            s = 0;
-            s += hrm[x[0]];
-            ii = 0;
-            while True:
-                try:
-                    if y[ii] == 1: s += hrm[x[ii+1]];
-                    if y[ii] == 2: s -= hrm[x[ii+1]];
-                    if y[ii] == 3: s = s * hrm[x[ii+1]];
-                    if y[ii] == 4: s = s / hrm[x[ii+1]];
-                except:
-                    break;
-                ii += 1;
+def countMath(n1,n2,n3,n4,n5,n6,tar):
+    signs = [1,2,3,4];
+    numbs = [[1,2,3,4,5,6],7];
+    hrn = {1:"+", 2:"-", 3:"*", 4:"/"};
+    hrm = {1:eval(n1), 2:eval(n2), 3:eval(n3), 4:eval(n4), 5:eval(n5), 6:eval(n6), 7:eval(tar)};
+    sol = [];
+    i = 0;
+    while i != 6:
+        for x in itertools.permutations(numbs[0], i+1):
+            x = list( x );
+            for y in itertools.product(signs, repeat=i):
+                s = 0;
+                s += hrm[x[0]];
+                ii = 0;
+                while True:
+                    try:
+                        if y[ii] == 1: s += hrm[x[ii+1]];
+                        if y[ii] == 2: s -= hrm[x[ii+1]];
+                        if y[ii] == 3: s = s * hrm[x[ii+1]];
+                        if y[ii] == 4: s = s / hrm[x[ii+1]];
+                    except:
+                        break;
+                    ii += 1;
 
-            if s == hrm[numbs[1]]:
-                sol.append([[ hrn[iii] for iii in y], [hrm[iii] for iii in x]]);
-    i += 1;
-
-print ( "there are", len ( sol ),"solutions" );
+                if s == hrm[numbs[1]]:
+                    sol.append([[ hrn[iii] for iii in y], [hrm[iii] for iii in x]]);
+        i += 1;
+    return sol;
 
 
 
